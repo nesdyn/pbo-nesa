18b200f1fadf9c0650f0565d5b4f886bdbbec406
nesdyn <vvlcaramel@gmail.com>
2023-08-08 08:09:22 +0700

    firstcommit

---
+++ Views/persegi.php
@@ -0,0 +1,9 @@
+<?php
+require('../Class/class_perhitungan.php');
+
+$hitung= new perhitungan();
+
+echo $hitung->luasPersegi(10);
+
+
+?>
---
+++ Views/segitiga.php
@@ -0,0 +1,8 @@
+<?php
+require('../Class/class_perhitungan.php');
+
+$hitung = new perhitungan();
+
+echo $hitung->luasSegitiga(10,7,7);
+
+?>
